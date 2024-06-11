ecbcad38d9c010ed9d9638c90164e6d43a883730
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-11 16:27:29 -0300

    [Luiz Henrique] Projeto Inventory Report

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
-def test_create_product() -> None:
-    raise NotImplementedError
+from inventory_report.product import Product
+
+
+def test_create_product():
+    product = Product(
+            "1",
+            "product",
+            "company",
+            "01/01/2021",
+            "01/01/2022",
+            "123",
+            "keep it cool",
+        )
+    assert product.id == "1"
+    assert product.product_name == "product"
+    assert product.company_name == "company"
+    assert product.manufacturing_date == "01/01/2021"
+    assert product.expiration_date == "01/01/2022"
+    assert product.serial_number == "123"
+    assert product.storage_instructions == "keep it cool"
