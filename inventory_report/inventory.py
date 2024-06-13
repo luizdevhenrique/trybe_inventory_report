43b015622ad328e3be04c5ccdd62d9b49e5071c0
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-13 15:33:43 -0300

    requisito 5 feito

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,16 @@
+from typing import List, Optional
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        if data is None:
+            data = []
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data.copy()
+
+    def add_data(self, data: List[Product]):
+        self._data.extend(data)
