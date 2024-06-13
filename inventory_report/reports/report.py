fa3f9a6adcde63dc70d14d4772b5ca6fface434d
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-13 16:57:41 -0300

    fazendo o requisito 7

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -4,7 +4,7 @@ from inventory_report.inventory import Inventory
 
 class Report(Protocol):
     def add_inventory(self, inventory: Inventory) -> None:
-        ...
+        pass
 
     def generate(self) -> str:
-        ...
+        pass
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,36 @@
-class SimpleReport:
-    pass
+from datetime import datetime
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self) -> None:
+        self.inventories = []
+
+    def add_inventory(self, inventory: Inventory) -> None:
+        self.inventories.append(inventory)
+
+    def generate(self) -> str:
+        oldest_manufacturing = min(
+            datetime.strptime(product.manufacturing_date, '%Y-%m-%d')
+            for inventory in self.inventories
+            for product in inventory.data
+        )
+
+        closest_expiration_date = min(
+            datetime.strptime(product.expiration_date, '%Y-%m-%d')
+            for inventory in self.inventories
+            for product in inventory.data if datetime.strptime(
+                product.expiration_date, '%Y-%m-%d')
+            > datetime.now()
+        )
+        company_with_largest_inventory = max(
+            self.inventories, key=lambda inventory: len(inventory.data)
+        ).data[0].company_name
+
+        return (
+            f"Oldest manufacturing date: {oldest_manufacturing}\n "
+            f"Closest expiration date: {closest_expiration_date}\n "
+            f"Company with the largest "
+            f"inventory: {company_with_largest_inventory}"
+        )
